6207e885d7a44e7c5e7bcc4c14e98e9da176eda7
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-09-10 10:23:36 +0300

    moved files with resources to funcs

--- clientconfig/requests_test.go
+++ clientconfig/requests_test.go
@@ -2,8 +2,6 @@ package clientconfig
 
 import (
 	"fmt"
-	"github.com/flosch/pongo2"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 
@@ -27,125 +25,117 @@ func TestGetCloudFromYAML_emptyAll(t *testing.T) {
 }
 
 func TestGetCloudFromPublic(t *testing.T) {
-	cloudsYamlPath := fmt.Sprintf("resources/%s.yaml", utils.RandomString(10, "clouds"))
-	cloudsPublicYamlPath := fmt.Sprintf("resources/%s.yaml", utils.RandomString(15, "clouds-public"))
+	cloudsYamlPath := fmt.Sprintf("/tmp/%s.yaml", utils.RandomString(10, "clouds"))
+	cloudsPublicYamlPath := fmt.Sprintf("/tmp/%s.yaml", utils.RandomString(15, "clouds-public"))
 
 	_ = os.Setenv("OS_CLIENT_CONFIG_FILE", cloudsYamlPath)
 	_ = os.Setenv("OS_CLIENT_VENDOR_FILE", cloudsPublicYamlPath)
 	_ = os.Setenv("OS_CLOUD", "test")
 
-	cloudsTemplate, err := pongo2.FromFile("resources/clouds.yaml.j2")
-	require.NoError(t, err)
+	var osAuthUrl = "http://url-from-clouds.yaml"
+	var osProjectName = "eu-de"
+	var osUsername = "otc"
+	var osPassword = "Qwerty123!"
+	var osDomainName = "OTC987414257102518"
+	cloudsTemplate := cloudsYamlTemplate(osAuthUrl, osProjectName, osUsername, osPassword, osDomainName)
 
-	cloudsPublicTemplate, err := pongo2.FromFile("resources/clouds-public.yaml.j2")
-	require.NoError(t, err)
-
-	out, err := cloudsTemplate.Execute(pongo2.Context{
-		"os_auth_url":     "http://url-from-clouds.yaml",
-		"os_project_name": "eu-de",
-		"os_username":     "otc",
-		"os_password":     "Qwerty123!",
-		"os_domain_name":  "OTC987414257102518"})
-	require.NoError(t, err)
+	var osAuthUrlPublic = "http://url-from-clouds-public.yaml"
+	cloudsPublicTemplate := cloudsPublicYamlTemplate(osAuthUrlPublic)
 
 	f, err := os.Create(cloudsYamlPath)
 	require.NoError(t, err)
 
 	defer f.Close()
 
-	_, err = f.Write([]byte(out))
-	require.NoError(t, err)
-
-	out, err = cloudsPublicTemplate.Execute(pongo2.Context{
-		"os_auth_url": "http://url-from-clouds-public.yaml",
-	})
+	_, err = f.Write([]byte(cloudsTemplate))
 	require.NoError(t, err)
 
 	f, err = os.Create(cloudsPublicYamlPath)
 	require.NoError(t, err)
-
-	_, err = f.Write([]byte(out))
+	_, err = f.Write([]byte(cloudsPublicTemplate))
 	require.NoError(t, err)
-
-	assert.NoError(t, f.Close())
 
 	cl, err := GetCloudFromYAML(&ClientOpts{})
 	require.NoError(t, err)
 	require.Contains(t, cl.AuthInfo.AuthURL, "http://url-from-clouds-public.yaml")
 	require.Contains(t, cl.AuthInfo.Password, "Qwerty123!")
-
-	//err = os.Remove(cloudsYamlPath)
-	//require.NoError(t, err)
-	//err = os.Remove(cloudsPublicYamlPath)
-	//require.NoError(t, err)
 }
 
 func TestGetCloudFromAllClouds(t *testing.T) {
-	cloudsYamlPath := fmt.Sprintf("resources/%s.yaml", utils.RandomString(10, "clouds"))
-	cloudsPublicYamlPath := fmt.Sprintf("resources/%s.yaml", utils.RandomString(15, "clouds-public"))
-	cloudsSecureYamlPath := fmt.Sprintf("resources/%s.yaml", utils.RandomString(10, "secure"))
+	cloudsYamlPath := fmt.Sprintf("/tmp/%s.yaml", utils.RandomString(10, "clouds"))
+	cloudsPublicYamlPath := fmt.Sprintf("/tmp/%s.yaml", utils.RandomString(15, "clouds-public"))
+	cloudsSecureYamlPath := fmt.Sprintf("/tmp/%s.yaml", utils.RandomString(10, "secure"))
 
 	_ = os.Setenv("OS_CLIENT_CONFIG_FILE", cloudsYamlPath)
 	_ = os.Setenv("OS_CLIENT_VENDOR_FILE", cloudsPublicYamlPath)
 	_ = os.Setenv("OS_CLIENT_Secure_FILE", cloudsSecureYamlPath)
 	_ = os.Setenv("OS_CLOUD", "test")
 
-	cloudsTemplate, err := pongo2.FromFile("resources/clouds.yaml.j2")
-	require.NoError(t, err)
+	var osAuthUrl = "http://url-from-clouds.yaml"
+	var osProjectName = "eu-de"
+	var osUsername = "otc"
+	var osPassword = "Qwerty123!"
+	var osDomainName = "OTC987414257102518"
+	cloudsTemplate := cloudsYamlTemplate(osAuthUrl, osProjectName, osUsername, osPassword, osDomainName)
 
-	cloudsPublicTemplate, err := pongo2.FromFile("resources/clouds-public.yaml.j2")
-	require.NoError(t, err)
+	var osAuthUrlPublic = "http://url-from-clouds-public.yaml"
+	cloudsPublicTemplate := cloudsPublicYamlTemplate(osAuthUrlPublic)
 
-	cloudsSecureTemplate, err := pongo2.FromFile("resources/secure.yaml.j2")
-	require.NoError(t, err)
-
-	out, err := cloudsTemplate.Execute(pongo2.Context{
-		"os_auth_url":     "http://url-from-clouds.yaml",
-		"os_project_name": "eu-de",
-		"os_username":     "otc",
-		"os_password":     "Qwerty123!",
-		"os_domain_name":  "OTC987414257102518"})
-	require.NoError(t, err)
+	var password = "SecuredPa$$w0rd1@"
+	cloudsSecureTemplate := cloudsSecureYamlTemplate(password)
 
 	f, err := os.Create(cloudsYamlPath)
 	require.NoError(t, err)
 
 	defer f.Close()
 
-	_, err = f.Write([]byte(out))
-	require.NoError(t, err)
-
-	out, err = cloudsPublicTemplate.Execute(pongo2.Context{
-		"os_auth_url": "http://url-from-clouds-public.yaml",
-	})
+	_, err = f.Write([]byte(cloudsTemplate))
 	require.NoError(t, err)
 
 	f, err = os.Create(cloudsPublicYamlPath)
 	require.NoError(t, err)
-
-	_, err = f.Write([]byte(out))
-	require.NoError(t, err)
-
-	out, err = cloudsSecureTemplate.Execute(pongo2.Context{
-		"os_password": "SecuredPa$$w0rd1@",
-	})
+	_, err = f.Write([]byte(cloudsPublicTemplate))
 	require.NoError(t, err)
 
 	f, err = os.Create(cloudsSecureYamlPath)
 	require.NoError(t, err)
-
-	_, err = f.Write([]byte(out))
+	_, err = f.Write([]byte(cloudsSecureTemplate))
 	require.NoError(t, err)
 
 	cl, err := GetCloudFromYAML(&ClientOpts{})
 	require.NoError(t, err)
 	require.Contains(t, cl.AuthInfo.AuthURL, "http://url-from-clouds-public.yaml")
 	require.Contains(t, cl.AuthInfo.Password, "SecuredPa$$w0rd1")
+}
 
-	//err = os.Remove(cloudsYamlPath)
-	//require.NoError(t, err)
-	//err = os.Remove(cloudsPublicYamlPath)
-	//require.NoError(t, err)
-	//err = os.Remove(cloudsSecureYamlPath)
-	//require.NoError(t, err)
+func cloudsYamlTemplate(authUrl, projectName, userName, domainName, password string) string {
+	return fmt.Sprintf(`
+clouds:
+  test:
+    profile: "otc"
+    auth:
+      auth_url: "%s"
+      project_name: "%s"
+      username: "%s"
+      user_domain_name: "%s"
+      password: "%s"
+`, authUrl, projectName, userName, domainName, password)
+}
+
+func cloudsPublicYamlTemplate(authUrl string) string {
+	return fmt.Sprintf(`
+public-clouds:
+  otc:
+    auth:
+      auth_url: "%s"
+`, authUrl)
+}
+
+func cloudsSecureYamlTemplate(password string) string {
+	return fmt.Sprintf(`
+clouds:
+  test:
+    auth:
+      password: "%s"
+`, password)
 }
