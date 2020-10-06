a996d5f9b50d04b0c9fb3182e4888af8b577207b
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-10-06 11:53:36 +0300

    Remove unused fields

--- clientconfig/requests.go
+++ clientconfig/requests.go
@@ -40,9 +40,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// AuthType represents a valid method of authentication.
-type AuthType string
-
 // ClientOpts represents options to customize the way a client is
 // configured.
 type ClientOpts struct {
@@ -51,10 +48,6 @@ type ClientOpts struct {
 
 	// EnvPrefix allows a custom environment variable prefix to be used.
 	EnvPrefix string
-
-	// AuthType specifies the type of authentication to use.
-	// By default, this is "password".
-	AuthType AuthType
 
 	// AuthInfo defines the authentication information needed to
 	// authenticate to a cloud when clouds.yaml isn't used.
